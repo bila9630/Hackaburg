4d30d796978907bd852e34302d4cd8948b95446e
Viet Duc Kieu <viet.duc.kieu@web.de>
2023-05-27 10:47:23 +0200

    add node button

--- pages/tree.tsx
+++ pages/tree.tsx
@@ -1,6 +1,7 @@
-import { Button } from '@mantine/core';
+import { Button, Center, Group, Modal, Space, TextInput } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { useRouter } from 'next/router';
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import ReactFlow, {
     MiniMap,
     Controls,
@@ -25,8 +26,24 @@ const Tree = () => {
     const router = useRouter()
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
     const onConnect = useCallback((params: any) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+
+    const [opened, { open, close }] = useDisclosure(false);
+    const [nodeName, setNodeName] = useState("");
+
+    const handleAddNote = useCallback((label: any) => {
+        const newNode = {
+            id: (nodes.length + 1).toString(),
+            position: {
+                x: 0,
+                y: 0,
+            },
+            data: { label: label },
+        };
+        setNodes((n) => n.concat(newNode));
+    }, [setNodes, nodes]);
+
+
 
     return (
         <div style={{ width: '100vw', height: '90vh' }}>
@@ -38,15 +55,48 @@ const Tree = () => {
                 onConnect={onConnect}
             >
                 <Controls />
-                <MiniMap />
+                {/* <MiniMap /> */}
                 <Background gap={12} size={1} />
-                <Panel position="bottom-center" style={{ marginBottom: "2rem" }}>
-                    {/* <Button onClick={addNode}>Add node</Button> */}
-                    <Button onClick={() => router.push("/listing")}>
-                        Add a posting
-                    </Button>
+                <Panel position="bottom-center" style={{ marginBottom: "3rem" }}>
+                    <Group>
+                        <Button
+                            variant="gradient"
+                            gradient={{ from: 'indigo', to: 'cyan' }}
+                            onClick={open}
+                            style={{ width: '150px' }}
+                        >
+                            Add node
+                        </Button>
+                        <Button
+                            variant="gradient"
+                            gradient={{ from: 'teal', to: 'lime', deg: 105 }}
+                            onClick={() => router.push("/listing")}
+                            style={{ width: '150px' }}
+                        >
+                            Add a posting
+                        </Button>
+                    </Group>
                 </Panel>
             </ReactFlow>
+
+            <Modal opened={opened} onClose={close} title="Add Note" centered>
+                <TextInput
+                    placeholder="Enter node name"
+                    label="Node name"
+                    value={nodeName}
+                    onChange={(event) => setNodeName(event.currentTarget.value)}
+                    withAsterisk
+                />
+                <Space h={20} />
+                <Center>
+                    <Button onClick={() => {
+                        handleAddNote(nodeName)
+                        setNodeName("")
+                        close()
+                    }}>Add note</Button>
+                </Center>
+            </Modal>
+
         </div>
     )
 }
